7ad42df8aa2e5ee5ebd16900e9ddf30db91b10c4
Adyasha Nanda <adyashananda105@gmail.com>
2025-10-22 22:05:59 +0530

    added the client connection

--- server/index.js
+++ server/index.js
@@ -1,22 +1,32 @@
-import dotenv from "dotenv";
 import express from "express";
-import {WebSocketServer} from "ws";
+import { WebSocketServer } from "ws";
+import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
+
+// Serve client folder
+app.use(express.static(path.resolve('../client')));
 
 const serverInstance = app.listen(PORT, () => {
-    console.log(`server is listening on ${PORT}`);
+  console.log(`Server is listening on ${PORT}`);
 });
 
 const wss = new WebSocketServer({ server: serverInstance });
 
-
 wss.on("connection", (ws) => {
-    ws.on("message", (data) => {
-        console.log(`Received message: ${data}`);
-        ws.send("MIL GAYA BROOOO");
-    })
-    console.log("client connected"); 
+  console.log("Client connected");
+
+  ws.on("message", (data) => {
+    const msg = data.toString();
+    console.log(`Client says: ${msg}`);
+    ws.send(`Server received: "${msg}"`);
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected ");
+  });
 });
