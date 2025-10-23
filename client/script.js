0fffcf71f172318e28da75bc60c6b6c35d24886f
Adyasha Nanda <adyashananda105@gmail.com>
2025-10-23 22:00:06 +0530

    added the url in script.js

--- client/script.js
+++ client/script.js
@@ -2,14 +2,14 @@
 const sender = prompt("Enter your name:");
 const room = prompt("Enter room name to join:");
 
-const socket = new WebSocket(`ws://localhost:8080?room=${room}`); //
+const socket = new WebSocket(`ws://websocket-wss-pfzh.onrender.com/?room=${room}`); //
 
 const messagesDiv = document.getElementById("messages");
 const input = document.getElementById("msgInput");
 const button = document.getElementById("sendBtn");
 
 // Load previous messages
-fetch(`http://localhost:8080/messages/${room}`)
+fetch(`https://websocket-wss-pfzh.onrender.com/messages/${room}`)
   .then(res => res.json())
   .then(data => {
     data.forEach(m => appendMessage(`${m.sender}: ${m.text}`));
