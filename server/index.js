4477153cf54ab059f274e79ac711b1b99c9faaa0
Adyasha Nanda <adyashananda105@gmail.com>
2025-10-23 12:13:22 +0530

    added a get route

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+  res.send("WebSocket server is running");
+});
+
 const serverInstance = app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
